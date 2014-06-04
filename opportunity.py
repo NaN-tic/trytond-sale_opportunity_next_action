f4227450a398d25672794a4071767821528d1098
Guillem Barba <guillem@nan-tic.com>
2014-06-04 10:33:11 +0200

    PEP8 formatting

--- opportunity.py
+++ opportunity.py
@@ -1,13 +1,10 @@
-#This file is part of sale_opportunity_quote module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
 from trytond.model import fields
 from trytond.pool import PoolMeta
 
-__metaclass__ = PoolMeta
-
 __all__ = ['SaleOpportunity']
+__metaclass__ = PoolMeta
 
 
 class SaleOpportunity:
@@ -15,5 +12,3 @@ class SaleOpportunity:
 
     next_action_date = fields.DateTime('Next Action Date')
     next_action = fields.Char('Next Action')
-
-
